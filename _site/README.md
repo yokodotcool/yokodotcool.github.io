8129c5c6cc9c45ef23cd7ad12d07b0363f1eda96
psyoko <ysohama@gmail.com>
2018-06-20 21:05:38 -0400

    Modifies gitignore, removes cached files

--- _site/README.md
+++
@@ -1,8 +0,0 @@
-# README file for yoko.is
-
-Wow everyone can read this and that’s cool ¯\\\_(ツ)\_/¯
-
-### Todos lol
-- Better treatments for images
-- Better typescale for post content
-- Add “ongoing” section to Archive page
--- _site/js/facts.js
+++
@@ -1,8 +0,0 @@
-Friendly but not super social
-Prefers cats over dogs by a landslide
-Undecided on the presence of a higher being
-NYC native (Upper West Side)
-Likes pandas, like a lot
-Inscrutably particular about sweet foods
-Awful at watching movies and TV
-Stopping wearing colors in January 2015
